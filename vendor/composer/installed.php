7357b61a79632887b72c52506f3e7bc5586d049e
Edgaras <edgaras.tkacenka@wallee.com>
2024-09-04 10:21:30 +0300

    Release 1.0.29

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '23881441945da62c99ed76e8ccae1abaa8c573cc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '23881441945da62c99ed76e8ccae1abaa8c573cc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wallee/sdk' => array(
+            'pretty_version' => '3.2.0',
+            'version' => '3.2.0.0',
+            'reference' => 'd9499a9c268ea6623858c0b6e16fc5ee96bac444',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wallee/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
