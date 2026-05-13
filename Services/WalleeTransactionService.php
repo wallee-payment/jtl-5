cf28826dedb407a0c5d7a9a8a3c879ed9b23b8de
andrewrowanwallee <andrew.rowan@wallee.com>
2026-05-13 11:42:24 +0200

    Release 1.0.45

--- Services/WalleeTransactionService.php
+++ Services/WalleeTransactionService.php
@@ -178,6 +178,7 @@ class WalleeTransactionService
             $orderId = (int) $orderData->kBestellung;
             // We tell to wawi do not synchronise it, because it's not paid yet. Wawi do not have pending state, so, we have to tell that it's already synchronised.
             // Later, when payment is accepted, we change the flag to N and ask Wawi to synchronise it.
+            WalleeHelper::log("confirmTransaction: Order $orderId created. Setting cAbgeholt to NOT_SYNC_TO_WAWI ('" . self::NOT_SYNC_TO_WAWI . "').");
             $this->updateWawiSyncFlag($orderId, self::NOT_SYNC_TO_WAWI);
             $order->data = json_encode([]);
             $order->order_id = $orderId;
@@ -901,6 +902,7 @@ class WalleeTransactionService
      * @return void
      */
     public function updateWawiSyncFlag(int $orderId, $flag) {
+        WalleeHelper::log("updateWawiSyncFlag: Updating Order $orderId cAbgeholt to '$flag'.");
         Shop::Container()
             ->getDB()->update(
             'tbestellung',
